cca7c009b9de8748577819c7172704bcc3890cbb
Abhinav Shahi <abhinavshahii06@gmail.com>
2025-05-21 11:23:35 +0545

    slide 1

---
+++ lecture1/slide1/functions.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void meow(void);
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        meow();
+    }
+}
+
+// Meow once
+void meow(void)
+{
+    printf("meow\n");
+}
+
+int get_positive_int(void);
+void meow(int n);
+
+int main(void)
+{
+    int n = get_positive_int();
+    meow(n);
+}
+
+// Get number of meows
+int get_positive_int(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number: ");
+    }
+    while (n < 1);
+    return n;
+}
+
+// Meow some number of times
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+
+}
+
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
